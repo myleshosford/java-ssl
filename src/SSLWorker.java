ec11697098f18be97457280be97ae02bae7d8365
myles <myles@192.168.143.1>
2013-11-22 09:58:36 +0000

    myles latest commit

---
+++ src/SSLWorker.java
@@ -0,0 +1,20 @@
+
+import javax.swing.SwingWorker;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author user
+ */
+public class SSLWorker extends SwingWorker<Void, Integer> {
+
+    
+    public  Void doInBackground() {
+        return null;
+    }
+    
+}
